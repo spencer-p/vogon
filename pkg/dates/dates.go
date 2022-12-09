5160126f4b7656082e07b0bb4a0911eb04993505
Spencer Peterson <spencerjpeterson@gmail.com>
2022-12-08 23:03:33 -0800

    Forgot to add relative dates package

---
+++ pkg/dates/dates.go
@@ -0,0 +1,52 @@
+// Package dates provides some human friendly relative date functions.
+package dates
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func weekday(day time.Weekday) func(now time.Time) time.Time {
+	return func(now time.Time) time.Time {
+		for now.Weekday() != day {
+			now = now.AddDate(0, 0, 1)
+		}
+		return now
+	}
+}
+
+var (
+	Monday    = weekday(time.Monday)
+	Tuesday   = weekday(time.Tuesday)
+	Wednesday = weekday(time.Wednesday)
+	Thursday  = weekday(time.Thursday)
+	Friday    = weekday(time.Friday)
+	Saturday  = weekday(time.Saturday)
+	Sunday    = weekday(time.Sunday)
+)
+
+func ParseRelative(now time.Time, date string) (time.Time, error) {
+	switch strings.ToLower(date) {
+	case "tomorrow", "tom":
+		return now.AddDate(0, 0, 1), nil
+	case "monday", "mon":
+		return Monday(now), nil
+	case "tuesday", "tue":
+		return Tuesday(now), nil
+	case "wednesday", "wed":
+		return Wednesday(now), nil
+	case "thursday", "thu":
+		return Thursday(now), nil
+	case "friday", "fri":
+		return Friday(now), nil
+	case "saturday", "sat":
+		return Saturday(now), nil
+	case "sunday", "sun":
+		return Sunday(now), nil
+	case "today", "tod":
+		return now, nil
+	default:
+		return time.Time{}, fmt.Errorf("unknown datestring: %q", date)
+	}
+}
